9ace89053aa3ef5e60a913278b25a8691ed64c14
tit-OliwierSz <64870510+tit-OliwierSz@users.noreply.github.com>
2020-06-05 21:00:29 +0200

    Add files via upload

---
+++ zadanie3/skrypt.js
@@ -0,0 +1,48 @@
+function zmien1()
+{
+	document.getElementById('foto1').src="zdj1.jpg";
+}
+function zmien2()
+{
+	document.getElementById('foto1').src="zdj2.jpg";
+}
+function zmien3()
+{
+	document.getElementById('foto2').src="zdj3.jpg";
+}
+function zmien4()
+{
+	document.getElementById('foto2').src="zdj4.jpg";
+}
+function zmien5()
+{
+	document.getElementById('foto3').src="zdj5.jpg";
+}
+function zmien6()
+{
+	document.getElementById('foto3').src="zdj6.jpg";
+}
+function zmien7()
+{
+	document.getElementById('foto4').src="zdj7.jpg";
+}
+function zmien8()
+{
+	document.getElementById('foto4').src="zdj8.jpg";
+}
+function zmien9()
+{
+	document.getElementById('foto5').src="zdj9.jpg";
+}
+function zmien10()
+{
+	document.getElementById('foto5').src="zdj10.jpg";
+}
+function zmien11()
+{
+	document.getElementById('foto6').src="zdj11.jpg";
+}
+function zmien12()
+{
+	document.getElementById('foto6').src="zdj12.jpg";
+}
